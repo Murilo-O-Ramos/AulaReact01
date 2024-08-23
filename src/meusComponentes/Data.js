08ca38159176893fb1a08990394092d8f6d374c3
Murilo-O-Ramos <murilooramos@hotmail.com>
2024-08-23 08:57:00 -0300

    corrigindo o funcionamento do componente data

--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -14,9 +14,9 @@ export default class Data extends Component{
         const dataAtual = new Date();
         let timeZoneFromDB = parseInt(timeZone); 
         let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
-        let milisecundos = parseInt(dataAtual.getTime() + diferencaTempo * 60 * 1000)
-        console.log(milisecundos)
-        return Date(milisecundos);
+        let milisecundos = parseInt(dataAtual.getTime() + (diferencaTempo * 60 * 1000));
+        const data = new Date(milisecundos);
+        return data
     }
 
     //fase de montagem
